b0689802f3658acfa1ecd11530963d5874e03a5c
Amalraj19 <84272989+Amalraj19@users.noreply.github.com>
2021-05-24 22:38:15 +0530

    Add files via upload

---
+++ patterns.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<cs50.h>
+int main(void){
+    int i, j, n, m;
+    n = get_int("Enter the number (1 or 2): ");
+    m = get_int("Number of rows: ");
+        if(n==1)
+        {
+            for(i=1; i<=m; ++i)
+            {
+                printf("\n");
+                for(j=m; j>=i; --j)
+                {
+                    printf("#");
+                    if(j==1)
+                    break;
+                }
+            }
+        }
+else
+{
+    for(i=1; i<=m; ++i)
+    {
+              printf("\n");
+        for(j=1; j<=i; ++j)
+        {
+            printf("#");
+            if(j==m)
+            break;
+        }
+    }
+}
+    return 0;
+}
