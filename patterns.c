65dc6728c0bff74a144642d4949d0f15e6e3255e
Amalraj19 <84272989+Amalraj19@users.noreply.github.com>
2021-05-25 22:45:18 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -1,38 +1,65 @@
 #include<stdio.h>
+
 #include<cs50.h>
+
 int main(void){
+
     int i, j, n, m;
+
     n = get_int("Enter the number (1 or 2): ");
+
     m = get_int("Number of rows: ");
-    { if(n==1)
+
+        if(n==1)
+
         {
-            for(i=1; i<=m; i++)
+
+            for(i=1; i<=m; ++i)
+
             {
-            for(j=m; j>=i; j--)
-            {
-            printf("#");
+
+                printf("\n");
+
+                for(j=m; j>=i; --j)
+
+                {
+
+                    printf("#");
+
+                }
+
             }
-            for(j=1; j<=i; j++)
-            {
-            printf(" ");
-            }
-            printf("\n");
-            }
+
         }
+
 else
+
 {
+
     for(i=1; i<=m; i++)
+
     {
+
         for(j=m; j>=i; j--)
+
         {
+
         printf(" ");
+
         }
+
         for(j=1; j<=i; j++)
+
         {
+
             printf("#");
+
         }
+
         printf("\n");
+
     }
+
 }
-}
+
 }
